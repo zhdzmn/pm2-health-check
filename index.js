93417576edeb7baac542f88685a618d452eb636b
A K M Zahiduzzaman <zhd.zmn@gmail.com>
2016-03-15 11:31:39 +0600

    ES5 support

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 module.exports = function (app, opts) {
   app.get(opts.url || '/health', function (req, res) {
     var pm2 =  require('pm2');
-    pm2.connect((err) =>  {
+    pm2.connect(function (err) {
       if (err) { res.sendStatus(500); return; }
-      pm2.list((err, processes) => {
+      pm2.list(function (err, processes) {
         pm2.disconnect();
         if (err) { res.sendStatus(500); return; }
-        var allOK;
+        var ok;
         if (opts.optimistic) {
-        	allOK = processes.every((process) => { return process.pm2_env.status === 'online'; });
+        	ok = processes.every((process) => { return process.pm2_env.status === 'online'; });
         } else {
-        	allOK = processes.some((process) => { return process.pm2_env.status === 'online'; });
+        	ok = processes.some((process) => { return process.pm2_env.status === 'online'; });
         }
-        res.sendStatus(allOK ? 200 : 500);
+        res.sendStatus(ok ? 200 : 500);
       });
     });
   });
